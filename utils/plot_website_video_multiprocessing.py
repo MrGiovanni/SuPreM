3c6334009c33504ae401fa063a86bc37aa1c9d54
Zongwei Zhou <zzhou82@jh.edu>
2024-03-13 01:15:01 -0400

    Update plot_website_video_multiprocessing.py

--- utils/plot_website_video_multiprocessing.py
+++ utils/plot_website_video_multiprocessing.py
@@ -1,5 +1,5 @@
 '''
-python -W ignore plot_website_video_multiprocessing.py --abdomen_atlas /Users/zongwei.zhou/Downloads/AbdomenAtlasPro --png_save_path /Users/zongwei.zhou/Downloads/AbdomenAtlasProPNG --video_save_path /Users/zongwei.zhou/Downloads/AbdomenAtlasProAVI --gif_sav
+python -W ignore plot_website_video_multiprocessing.py --abdomen_atlas /Users/zongwei.zhou/Downloads/AbdomenAtlasPro --png_save_path /Users/zongwei.zhou/Downloads/AbdomenAtlasProPNG --video_save_path /Users/zongwei.zhou/Downloads/AbdomenAtlasProAVI --gif_save_path /Users/zongwei.zhou/Downloads/AbdomenAtlasProGIF --FPS 25
 '''
 
 import numpy as np 
@@ -18,7 +18,7 @@ low_range = -150
 high_range = 250
 
 class_of_interest = ['aorta',
-                     'gall_bladder',
+                     'gallbladder',
                      'kidney_left',
                      'kidney_right',
                      'liver',
@@ -26,13 +26,18 @@ class_of_interest = ['aorta',
                      'postcava',
                      'spleen',
                      'stomach',
+                     'adrenal_gland_right',
+                     'adrenal_gland_left',
+                     'duodenum',
+                     'intestine',
+                     'colon',
                     ]
 
 CLASS_IND = {
     'spleen': 1,
     'kidney_right': 2,
     'kidney_left': 3,
-    'gall_bladder': 4,
+    'gallbladder': 4,
     'esophagus': 5,
     'liver': 6,
     'stomach': 7,
@@ -40,8 +45,8 @@ CLASS_IND = {
     'postcava': 9,
     'portal and splenic vein': 10,
     'pancreas': 11,
-    'adrenal gland R': 12,
-    'adrenal gland L': 13,
+    'adrenal_gland_right': 12,
+    'adrenal_gland_left': 13,
     'duodenum': 14,
     'hepatic vessel': 15,
     'lung R': 16,
@@ -65,18 +70,18 @@ CLASS_IND = {
 
 def add_colorful_mask(image, mask, class_index):
     
-    image[mask == class_index['spleen'], 0] = 255   # spleen (255,0,0) 
+    image[mask == class_index['spleen'], 0] = 255   # spleen (255,0,0)
     
     image[mask == class_index['kidney_right'], 1] = 255   # kidney_right (0,255,0)
     image[mask == class_index['kidney_left'], 1] = 255   # kidney_left (0,255,0)
     image[mask == class_index['kidney tumor'], 1] = 255  # kidney tumor (0,255,0)
     image[mask == class_index['kidney cyst'], 1] = 255  # kidney cyst (0,255,0)
     
-    image[mask == class_index['gall_bladder'], 0] = 255   # gall_bladder (255,255,0)
-    image[mask == class_index['gall_bladder'], 1] = 255   # 
+    image[mask == class_index['gallbladder'], 0] = 255   # gallbladder (255,255,0)
+    image[mask == class_index['gallbladder'], 1] = 255   #
     
-    # image[mask == class_index['esophagus'], 1] = 255   # esophagus (0,255,255)
-    # image[mask == class_index['esophagus'], 2] = 255   # 
+    image[mask == class_index['intestine'], 1] = 255   # esophagus (0,255,255)
+    image[mask == class_index['intestine'], 2] = 255   #
     image[mask == class_index['liver'], 0] = 255   # liver (255,0,255)
     image[mask == class_index['hepatic vessel'], 0] = 255  # hepatic vessel (255,0,255)
     image[mask == class_index['liver tumor'], 0] = 255  # liver tumors (255,0,255)
@@ -88,36 +93,36 @@ def add_colorful_mask(image, mask, class_index):
     
     image[mask == class_index['stomach'], 0] = 255
     image[mask == class_index['stomach'], 1] = 239   # stomach (255,239,255)
-    image[mask == class_index['stomach'], 2] = 213   # 
+    image[mask == class_index['stomach'], 2] = 213   #
     
     image[mask == class_index['aorta'], 1] = 255
     image[mask == class_index['aorta'], 2] = 255   # aorta (0,255,255)
     
     image[mask == class_index['postcava'], 0] = 205   # postcava (205,133,63)
-    image[mask == class_index['postcava'], 1] = 133   # 
-    image[mask == class_index['postcava'], 2] = 63 # + image[mask == class_index['postcava'], 2] * 0.2   # 
+    image[mask == class_index['postcava'], 1] = 133   #
+    image[mask == class_index['postcava'], 2] = 63 # + image[mask == class_index['postcava'], 2] * 0.2   #
     
     # image[mask == class_index['portal and splenic vein'], 0] = 0 + image[mask == class_index['portal and splenic vein'], 0] * 0.5 # portal and splenic vein (0,0,255)
-    # image[mask == class_index['portal and splenic vein'], 1] = 0 + image[mask == class_index['portal and splenic vein'], 1] * 0.5 # 
-    # image[mask == class_index['portal and splenic vein'], 2] = 255  # 
+    # image[mask == class_index['portal and splenic vein'], 1] = 0 + image[mask == class_index['portal and splenic vein'], 1] * 0.5 #
+    # image[mask == class_index['portal and splenic vein'], 2] = 255  #
     
     image[mask == class_index['pancreas'], 0] = 102  # pancreas (102,205,170)
     image[mask == class_index['pancreas'], 1] = 205
-    image[mask == class_index['pancreas'], 2] = 170  #  
+    image[mask == class_index['pancreas'], 2] = 170  #
     image[mask == class_index['pancreatic tumor'], 0] = 102  # pancreatic tumors (102,205,170)
     image[mask == class_index['pancreatic tumor'], 1] = 205
     image[mask == class_index['pancreatic tumor'], 2] = 170
 
-    # image[mask == class_index['adrenal gland R'], 0] = 0 + image[mask == class_index['adrenal gland R'], 0] * 0.5 # adrenal gland R (0,255,0)
-    # image[mask == class_index['adrenal gland R'], 1] = 255 # 
-    # image[mask == class_index['adrenal gland R'], 2] = 0 + image[mask == class_index['adrenal gland R'], 2] * 0.5  # 
-    # image[mask == class_index['adrenal gland L'], 0] = 0 + image[mask == class_index['adrenal gland L'], 0] * 0.5 # adrenal gland L (0,255,0)
-    # image[mask == class_index['adrenal gland L'], 1] = 255 # 
-    # image[mask == class_index['adrenal gland L'], 2] = 0 + image[mask == class_index['adrenal gland L'], 2] * 0.5 # 
+    image[mask == class_index['adrenal_gland_right'], 0] = 0 + image[mask == class_index['adrenal_gland_right'], 0] * 0.5 # adrenal_gland_right (0,255,0)
+    image[mask == class_index['adrenal_gland_right'], 1] = 255 #
+    image[mask == class_index['adrenal_gland_right'], 2] = 0 + image[mask == class_index['adrenal_gland_right'], 2] * 0.5  #
+    image[mask == class_index['adrenal_gland_left'], 0] = 0 + image[mask == class_index['adrenal_gland_left'], 0] * 0.5 # adrenal_gland_left (0,255,0)
+    image[mask == class_index['adrenal_gland_left'], 1] = 255 #
+    image[mask == class_index['adrenal_gland_left'], 2] = 0 + image[mask == class_index['adrenal_gland_left'], 2] * 0.5 #
     
-    # image[mask == class_index['duodenum'], 0] = 255 # duodenum (255,80,80)
-    # image[mask == class_index['duodenum'], 1] = 80 + image[mask == class_index['duodenum'], 1] * 0.6 # 
-    # image[mask == class_index['duodenum'], 2] = 80 + image[mask == class_index['duodenum'], 2] * 0.6 # 
+    image[mask == class_index['duodenum'], 0] = 255 # duodenum (255,80,80)
+    image[mask == class_index['duodenum'], 1] = 80 + image[mask == class_index['duodenum'], 1] * 0.6 #
+    image[mask == class_index['duodenum'], 2] = 80 + image[mask == class_index['duodenum'], 2] * 0.6 #
     
     # image[mask == class_index['lung R'], 0] = 200 + image[mask == class_index['lung R'], 0] * 0.2  # lung R (200,128,0)
     # image[mask == class_index['lung R'], 2] = 128 + image[mask == class_index['lung R'], 2] * 0.5  #
@@ -126,35 +131,46 @@ def add_colorful_mask(image, mask, class_index):
     # image[mask == class_index['lung tumor'], 0] = 200 + image[mask == class_index['lung tumor'], 0] * 0.2  # lung tumor (200,128,0)
     # image[mask == class_index['lung tumor'], 2] = 128 + image[mask == class_index['lung tumor'], 2] * 0.5  #
     
-    # image[mask == class_index['colon'], 0] = 170  # colon (170,0,255)
-    # image[mask == class_index['colon'], 1] = 0 + image[mask == class_index['colon'], 1] * 0.7    # 
-    # image[mask == class_index['colon'], 2] = 255  # 
-    # image[mask == class_index['colon tumor'], 0] = 170  # colon tumors (170,0,255)
-    # image[mask == class_index['colon tumor'], 1] = 0 + image[mask == class_index['colon tumor'], 1] * 0.7    # 
-    # image[mask == class_index['colon tumor'], 2] = 255  #
+    image[mask == class_index['colon'], 0] = 170  # colon (170,0,255)
+    image[mask == class_index['colon'], 1] = 0 + image[mask == class_index['colon'], 1] * 0.7    #
+    image[mask == class_index['colon'], 2] = 255  #
+    image[mask == class_index['colon tumor'], 0] = 170  # colon tumors (170,0,255)
+    image[mask == class_index['colon tumor'], 1] = 0 + image[mask == class_index['colon tumor'], 1] * 0.7    #
+    image[mask == class_index['colon tumor'], 2] = 255  #
     
     # image[mask == class_index['prostate'], 0] = 0    # prostate (0,128,128)
-    # image[mask == class_index['prostate'], 1] = 128  # 
-    # image[mask == class_index['prostate'], 2] = 128 + image[mask == class_index['prostate'], 2] * 0.5  # 
+    # image[mask == class_index['prostate'], 1] = 128  #
+    # image[mask == class_index['prostate'], 2] = 128 + image[mask == class_index['prostate'], 2] * 0.5  #
     
     # image[mask == class_index['celiac trunk'], 0] = 255    # celiac trunk (255,0,0)
-    # image[mask == class_index['celiac trunk'], 1] = 0 + image[mask == class_index['celiac trunk'], 1] * 0.5  # 
-    # image[mask == class_index['celiac trunk'], 2] = 0  # 
+    # image[mask == class_index['celiac trunk'], 1] = 0 + image[mask == class_index['celiac trunk'], 1] * 0.5  #
+    # image[mask == class_index['celiac trunk'], 2] = 0  #
     
     return image
 
+
 def load_individual_maps(segmentation_dir):
-    
-    mask_path = os.path.join(segmentation_dir, 'liver.nii.gz')
-    mask = nib.load(mask_path).get_fdata().astype(np.uint8)
+    # Initializes an empty mask array to hold the combined masks.
+    combined_mask = None
     
     for c in class_of_interest:
-        mask_path = os.path.join(segmentation_dir, c+'.nii.gz')
+        mask_path = os.path.join(segmentation_dir, c + '.nii.gz')
         c_mask = nib.load(mask_path).get_fdata().astype(np.uint8)
-        mask[c_mask == 1] = CLASS_IND[c]
+        
+        if combined_mask is None:
+            combined_mask = np.zeros_like(c_mask)
+        
+        combined_mask[c_mask == 1] = CLASS_IND[c]
     
-    return mask
-    
+    return combined_mask
+
+
+def find_roi_bounds(mask):
+    # Identifies the top and bottom z-values with masks for the ROI.
+    present_slices = np.any(mask, axis=(0, 1))
+    z_min, z_max = np.where(present_slices)[0][[0, -1]]
+    return z_min + 5, z_max - 5
+
 def full_make_png(case_name, args):
     
     for plane in ['axial', 'coronal', 'sagittal']:
@@ -167,7 +183,14 @@ def full_make_png(case_name, args):
 
     # single case
     image = nib.load(image_path).get_fdata().astype(np.int16)
+    # Load mask and find ROI bounds.
     mask = load_individual_maps(os.path.join(args.abdomen_atlas, case_name, 'segmentations'))
+    z_min, z_max = find_roi_bounds(mask)
+
+    # Modifications to handle the selected ROI instead of the whole volume.
+    for z in range(z_min, z_max + 1):
+        # Generate and save images for the ROI only.
+        pass  # Image generation and saving code here, adjusted for ROI.
     
     # change orientation
     ornt = nib.orientations.axcodes2ornt(('F', 'L', 'U'), (('L','R'),('B','F'),('D','U')))
