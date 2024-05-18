d592c473db2e7088a02a4b4fe77df9f0d8f05882
Wenxuan Li <44401292+WenxuanChelsea@users.noreply.github.com>
2024-05-17 22:23:04 -0400

    Update hg.sh

--- supervised_pretraining/hg.sh
+++ supervised_pretraining/hg.sh
@@ -36,11 +36,17 @@ source activate suprem
 # wordembeddingpath=./pretrained_weights/txt_encoding_abdomenatlas1.0.pth # for AbdomenAtlas 1.0
 
 # # Multiple GPUs
+# # Pre-train models with the swinunetr and the unet backbones
 # python -W ignore -m torch.distributed.launch --nproc_per_node=$nproc_per_node --master_port=$RANDOM_PORT train.py --dist  --data_root_path $datapath --dataset_list $datasetversion --num_workers $num_workers --log_name $datasetversion.$1 --pretrain $2 --word_embedding $wordembeddingpath --backbone $1 --lr 1e-4 --warmup_epoch 20 --batch_size $batch_size --max_epoch 1600 --cache_dataset --num_class 9 --cache_num $cache_num --dataset_version $datasetversion
 
 # # for backbone in unet; do for pretrainpath in ./pretrained_weights/Genesis_Chest_CT.pt; do sbatch --error=logs/$backbone.1.0.out --output=logs/$backbone.1.0.out hg.sh $backbone $pretrainpath; done; done
 
 # # for backbone in swinunetr; do for pretrainpath in ./pretrained_weights/swin_unetr.base_5000ep_f48_lr2e-4_pretrained.pt; do sbatch --error=logs/$backbone.1.0.out --output=logs/$backbone.1.0.out hg.sh $backbone $pretrainpath; done; done
+
+# # Pre-train models with the segresnet backbone
+# python -W ignore -m torch.distributed.launch --nproc_per_node=$nproc_per_node --master_port=$RANDOM_PORT train.py --dist --data_root_path $datapath --dataset_list $datasetversion --num_workers $num_workers --log_name $datasetversion.$1 --backbone $1 --lr 1e-4 --warmup_epoch 20 --batch_size $batch_size --max_epoch 1600 --cache_dataset --num_class 9 --cache_num $cache_num --dataset_version $datasetversion
+
+# # for backbone in segresnet; do sbatch --error=logs/$backbone.1.0.out --output=logs/$backbone.1.0.out hg.sh $backbone; done
 
 ### Training (AbdomenAtlas 1.1)
 
@@ -54,8 +60,15 @@ datasetversion=AbdomenAtlas1.1
 wordembeddingpath=./pretrained_weights/txt_encoding_abdomenatlas1.1.pth
 
 # Multiple GPUs
-python -W ignore -m torch.distributed.launch --nproc_per_node=$nproc_per_node --master_port=$RANDOM_PORT train.py --dist  --data_root_path $datapath --dataset_list $datasetversion --num_workers $num_workers --log_name $datasetversion.$1 --pretrain $2 --word_embedding $wordembeddingpath --backbone $1 --lr 1e-4 --warmup_epoch 20 --batch_size $batch_size --max_epoch 1600 --cache_dataset --num_class 25 --cache_num $cache_num --dataset_version $datasetversion
+# Pre-train models with the swinunetr and the unet backbones
+# python -W ignore -m torch.distributed.launch --nproc_per_node=$nproc_per_node --master_port=$RANDOM_PORT train.py --dist  --data_root_path $datapath --dataset_list $datasetversion --num_workers $num_workers --log_name $datasetversion.$1 --pretrain $2 --word_embedding $wordembeddingpath --backbone $1 --lr 1e-4 --warmup_epoch 20 --batch_size $batch_size --max_epoch 1600 --cache_dataset --num_class 25 --cache_num $cache_num --dataset_version $datasetversion
 
 # for backbone in unet; do for pretrainpath in ./pretrained_weights/Genesis_Chest_CT.pt; do sbatch --error=logs/$backbone.1.1.out --output=logs/$backbone.1.1.out hg.sh $backbone $pretrainpath; done; done
 
 # for backbone in swinunetr; do for pretrainpath in ./pretrained_weights/swin_unetr.base_5000ep_f48_lr2e-4_pretrained.pt; do sbatch --error=logs/$backbone.1.1.out --output=logs/$backbone.1.1.out hg.sh $backbone $pretrainpath; done; done
+
+# Pre-train models with the segresnet backbone
+python -W ignore -m torch.distributed.launch --nproc_per_node=$nproc_per_node --master_port=$RANDOM_PORT train.py --dist --data_root_path $datapath --dataset_list $datasetversion --num_workers $num_workers --log_name $datasetversion.$1 --backbone $1 --lr 1e-4 --warmup_epoch 20 --batch_size $batch_size --max_epoch 1600 --cache_dataset --num_class 25 --cache_num $cache_num --dataset_version $datasetversion
+
+# for backbone in segresnet; do sbatch --error=logs/$backbone.1.1.out --output=logs/$backbone.1.1.out hg.sh $backbone; done
+
