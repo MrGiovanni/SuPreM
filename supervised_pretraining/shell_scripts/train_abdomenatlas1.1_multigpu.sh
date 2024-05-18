4ad91e000891a816f99edc139832b08be26f9b55
Wenxuan Li <44401292+WenxuanChelsea@users.noreply.github.com>
2024-05-17 23:11:27 -0400

    Update train_abdomenatlas1.1_multigpu.sh

--- supervised_pretraining/shell_scripts/train_abdomenatlas1.1_multigpu.sh
+++ supervised_pretraining/shell_scripts/train_abdomenatlas1.1_multigpu.sh
@@ -42,4 +42,4 @@ datasetversion=AbdomenAtlas1.1 # or AbdomenAtlas1.0
 wordembeddingpath=./pretrained_weights/txt_encoding_abdomenatlas1.1.pth
 
 # Multiple GPUs
-python -W ignore -m torch.distributed.launch --nproc_per_node=$nproc_per_node --master_port=$RANDOM_PORT train.py --dist  --data_root_path $datapath --dataset_list $datasetversion --num_workers $num_workers --log_name $datasetversion.$1.multigpu --word_embedding $wordembeddingpath --backbone $1 --lr 1e-3 --warmup_epoch 20 --batch_size $batch_size --max_epoch 2000 --cache_dataset --num_class 9 --cache_num $cache_num --dataset_version $datasetversion
+python -W ignore -m torch.distributed.launch --nproc_per_node=$nproc_per_node --master_port=$RANDOM_PORT train.py --dist  --data_root_path $datapath --dataset_list $datasetversion --num_workers $num_workers --log_name $datasetversion.$1.multigpu --word_embedding $wordembeddingpath --backbone $1 --lr 1e-3 --warmup_epoch 20 --batch_size $batch_size --max_epoch 2000 --cache_dataset --num_class 25 --cache_num $cache_num --dataset_version $datasetversion
